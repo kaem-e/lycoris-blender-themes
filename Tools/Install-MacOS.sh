6c5ccebe929fd3dcc98dafb9155eaf0a50c4e52a
Kaem <46230985+kaem-e@users.noreply.github.com>
2022-08-07 08:26:11 +0500

    Fixed MacOS Install Script

--- Tools/Install-MacOS.sh
+++ Tools/Install-MacOS.sh
@@ -1,4 +1,28 @@
-#/bin/sh
+#!/bin/zsh
 
-cd ./..
-ln -sf ./Lycoris-\(dark\).xml ~/Library/Application Support/Blender/3.*/scripts/presets/interface_theme
+
+# StackOverflow my beloved
+() {
+  ln -f -- ./../Lycoris-dark.xml $argv[-1]/Lycoris-dark.xml
+} ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/
+
+
+
+
+
+
+
+
+
+
+# ln -sf ./Lycoris-\(dark\).xml ~/Library/Application Support/Blender/3.*/scripts/presets/interface_theme
+# cd ./..
+
+
+# () {
+#   ln -sf -- /./../Lycoris-dark $argv[-1]/interface-theme
+# } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/(/n)
+
+# ln -sf -- /./../Lycoris-dark ~/Library/Application Support/Blender/*/scripts/presets/interface_theme(/n[-1]e[REPLY+=/interface-theme])
+
+
