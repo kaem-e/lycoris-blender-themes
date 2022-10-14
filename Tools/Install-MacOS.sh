a8f8b74e871409c2f1fae021b2ffdde22b01916e
Kaem <46230985+kaem-e@users.noreply.github.com>
2022-10-14 07:28:38 +0500

    mac/lin scripts now able to make the inteface_theme dir

--- Tools/Install-MacOS.sh
+++ Tools/Install-MacOS.sh
@@ -2,8 +2,18 @@
 
 
 # StackOverflow my beloved
+# () {
+#   ln -Ffv -- ./../Lycoris-dark.xml $argv[-1]/Lycoris-dark.xml
+# } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/
+
+
+# StackOverflow my beloved
+# makes the interface_theme directory
 () {
-  ln -f -- ./../Lycoris-dark.xml $argv[-1]/Lycoris-dark.xml
+  mkdir $argv[-1]/interface_theme/
+} ~/Library/Application\ Support/Blender/*/scripts/presets/
+
+# Links the file(s)
+() {
+  ln -Ffv -- ./../Lycoris-dark.xml $argv[-1]/Lycoris-dark.xml
 } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/
-
-
