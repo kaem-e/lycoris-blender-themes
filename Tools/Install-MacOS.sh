956c6ff45fb9edc11ade4a36486fc71372b94622
Kaem <46230985+kaem-e@users.noreply.github.com>
2022-10-14 06:45:47 +0500

    removed junk rom macos script

--- Tools/Install-MacOS.sh
+++ Tools/Install-MacOS.sh
@@ -7,22 +7,3 @@
 } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/
 
 
-
-
-
-
-
-
-
-
-# ln -sf ./Lycoris-\(dark\).xml ~/Library/Application Support/Blender/3.*/scripts/presets/interface_theme
-# cd ./..
-
-
-# () {
-#   ln -sf -- /./../Lycoris-dark $argv[-1]/interface-theme
-# } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/(/n)
-
-# ln -sf -- /./../Lycoris-dark ~/Library/Application Support/Blender/*/scripts/presets/interface_theme(/n[-1]e[REPLY+=/interface-theme])
-
-
