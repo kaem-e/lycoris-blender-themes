2dd006edd85a7a0b4d7119dfd68486a94e64c267
Kaem <46230985+kaem-e@users.noreply.github.com>
2022-10-14 07:52:44 +0500

    made output text & improved mkdir command

--- Tools/Install-MacOS.sh
+++ Tools/Install-MacOS.sh
@@ -7,13 +7,26 @@
 # } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/
 
 
+# echo -e " "
+# echo "-----------------------------------------------------"
+# echo "Script will now link the file"
+# echo "if mkdir outputs \"File exists\" error don't worry its fine"
+# echo "-----------------------------------------------------"
+# echo -e " "
+
 # StackOverflow my beloved
 # makes the interface_theme directory
 () {
-  mkdir $argv[-1]/interface_theme/
+  mkdir -p $argv[-1]/interface_theme/
 } ~/Library/Application\ Support/Blender/*/scripts/presets/
 
 # Links the file(s)
 () {
-  ln -Ffv -- ./../Lycoris-dark.xml $argv[-1]/Lycoris-dark.xml
+  ln -Ff -- ./../Lycoris-dark.xml $argv[-1]/Lycoris-dark.xml
 } ~/Library/Application\ Support/Blender/*/scripts/presets/interface_theme/
+
+clear
+echo -e " "
+echo -e "Theme Install Completed \U1F984\n"
+echo -e "Now set as active theme in Blender Edit -> Preferences -> Themes -> Lycoris-(Light/Dark)"
+echo -e " "
